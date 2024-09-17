f4380d0cf235963cfa8e906391b2a550b7ce117b
Ichie Ozor <simeon_mc2000@yahoo.com>
2024-09-17 14:30:17 +0100

    ichie

--- app/main-body/DashBoard.js
+++ app/main-body/DashBoard.js
@@ -2,37 +2,45 @@ import React from "react";
 import { Text, View, FlatList, StyleSheet, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const data = [
   {
     id: 1,
     title: "Wallet Balance",
     amount: "N 5,000,000",
+    icon: <AntDesign name="wallet" size={40} color='#f5c005' />
   },
   {
     id: 2,
     title: "Vehicle",
     amount: "50",
+    icon: <AntDesign name="car" size={40} color='#f5c005' />
   },
   {
     id: 3,
     title: "My Transaction",
+    icon: <AntDesign name="book" size={40} color='#f5c005' />
   },
   {
     id: 4,
     title: "Top Up",
+    icon: <AntDesign name="creditcard" size={40} color='#f5c005' />
   },
   {
     id: 5,
     title: "My Vehicle",
+    icon: <AntDesign name="earth" size={40} color='#f5c005' />
   },
   {
     id: 6,
     title: "Fund Vehicle",
+    icon: <AntDesign name="bank" size={40} color='#f5c005' />
   },
   {
     id: 7,
     title: "Search Vehicle",
+    icon: <AntDesign name="creditcard" size={40} color='#f5c005' />
   },
 
 ];
@@ -50,6 +58,7 @@ export default function DashBoard() {
       style={styles.item}
     >
       <View>
+        <View>{item.icon}</View>
         <Text style={styles.itemText}>{item.title}</Text>
         <Text style={styles.itemAmount}>{item.amount ? item.amount : ""}</Text>
       </View>
@@ -59,7 +68,7 @@ export default function DashBoard() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.textHeader}>Welcome, Ishaq</Text>
-      <View>
+      <View style={styles.body}>
         <FlatList data={data} renderItem={items} numColumns={2} keyExtractor={(item) => item.id} />
       </View>
     </SafeAreaView>
@@ -72,32 +81,47 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
     marginTop: 0,
+    backgroundColor: '#f5c005',
     paddingHorizontal: 0,
+  },
+  body: {
+    backgroundColor: '#fffafa',
+    height: '100%'
   },
   textHeader: {
     fontSize: 20,
     fontWeight: '100',
+    borderBottomLeftRadius: 10,
+    marginTop: 0,
     marginBottom: 5,
     top: -12
   },
   item: {
     flex: 1,
-    margin: 2,
-    height: 120,
+    flexDirection: 'row',
+    borderRadius: 10,
+    margin: 10,
+    height: 100,
     borderCurve: 5,
     alignItems: 'center',
-    padding: 20,
-    backgroundColor: '#ff9900',
+    padding: 5,
+    borderColor: '#f5c005',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    shadowColor: '#ccc',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 10
   },
   itemText: {
-    color: 'white',
-    fontSize: 20,
+    color: 'black',
+    fontSize: 15,
     fontWeight: 'bold'
   },
   itemAmount: {
-    color: "white",
+    color: "black",
     fontSize: 14,
     fontWeight: 'semibold',
-    marginTop: 10
   },
 })
